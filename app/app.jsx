8556c150ad437ad6aa31652b680def8a66e6ea2e
trebek1 <trebek08@gmail.com>
2016-09-19 23:11:03 -0700

    got the x-axis labels working

--- app/app.jsx
+++ app/app.jsx
@@ -7,19 +7,45 @@ var LineChart = React.createClass({
     getInitialState:function(){
         return {
             width:800,
-            height: 300
+            height: 300,
+            tooltip:{ display:false,data:{key:'',value:''}}
+        	
         };
     },
+
+    showToolTip:function(e){
+	    e.target.setAttribute('fill', '#FFFFFF');
+	 
+	    this.setState({tooltip:{
+	        display:true,
+	        data: {
+	            key:e.target.getAttribute('data-key'),
+	            value:e.target.getAttribute('data-value')
+	            },
+	        pos:{
+	            x:e.target.getAttribute('cx'),
+	            y:e.target.getAttribute('cy')
+	        }
+	 
+	        }
+	    });
+	},
+	hideToolTip:function(e){
+	    e.target.setAttribute('fill', '#7dc7f4');
+	    this.setState({tooltip:{ display:false,data:{key:'',value:''}}});
+	},
+
     render:function(){
         var data=[
             {day:'02-11-2016',count:180},
             {day:'02-12-2016',count:250},
             {day:'02-13-2016',count:150},
-            {day:'02-14-2016',count:496},
+            {day:'02-14-2016',count:140},
             {day:'02-15-2016',count:140},
             {day:'02-16-2016',count:380},
             {day:'02-17-2016',count:100},
-            {day:'02-18-2016',count:150}
+            {day:'02-18-2016',count:150},
+            {day:'02-19-2016',count:496}
         ];
  
         var margin = {top: 5, right: 50, bottom: 20, left: 50},
@@ -27,17 +53,22 @@ var LineChart = React.createClass({
             h = this.state.height - (margin.top + margin.bottom);
  
         var parseDate = d3.timeParse("%m-%d-%Y");
- 
-        data.forEach(function (d) {
+        var formatTime = d3.timeFormat("%b %y")
+ 		data.forEach(function (d) {
             d.date = parseDate(d.day);
+            d.d = formatTime(d.date);
+            
+            
         });
- 
+
         var x = d3.scaleTime()
             .domain(d3.extent(data, function (d) {
                 return d.date;
             }))
             .rangeRound([0, w]);
- 
+
+            
+
         var y = d3.scaleLinear()
             .domain([0,d3.max(data,function(d){
                 return d.count+100;
@@ -52,6 +83,7 @@ var LineChart = React.createClass({
  
         var line = d3.line()
             .x(function (d) {
+
                 return x(d.date);
             })
             .y(function (d) {
@@ -59,20 +91,44 @@ var LineChart = React.createClass({
             }).curve(d3.curveCatmullRom.alpha(0.5));
  
         var transform='translate(' + margin.left + ',' + margin.top + ')';
-        
+
+
+        data.forEach(function (d) {
+            d.date = parseDate(d.day);
+            d.starcraft = x(d.date)
+          
+        });
+
+
         var yAxis = d3.axisLeft()
             .scale(y)
             .ticks(5)
             .tickSizeOuter([0])
 
- 
+        
 		var xAxis = d3.axisBottom()
 		   .scale(x)
 		   .tickValues(data.map(function(d,i){
-		       if(i>0)
-		           return d.date;
+		       		
+		       	return d.date;
+		       
+		        
 		   }).splice(1))
-		   .ticks(4);
+
+		   .tickFormat(function (d) {
+		  
+			  
+		  return d3.timeFormat('%a %d')(new Date(d))
+		})
+		 
+		 // .tickFormat(function (d) {
+			//   var mapper = {
+			//     "This": "This is long",
+			//     "That": "That is long",
+			//     "Other": "Other is long"
+			//   }
+			//   return mapper[d]
+			// })
 		 
 		var yGrid = d3.axisLeft()
 		   .scale(y)
@@ -87,13 +143,12 @@ var LineChart = React.createClass({
                 <svg id ='svg' width={this.state.width} height={this.state.height}>
  
                     <g transform={transform}>
-                        
-                        
                         <Grid h={h} grid={yGrid} gridType="y"/>
 						<Axis h={h} axis={yAxis} axisType="y" />
 						<Axis h={h} axis={xAxis} axisType="x"/>
                         <path className="line shadow" d={line(data)} strokeLinecap="round"/>
-                        <Dots data={data} x={x} y={y}/>
+                        <Dots data={data} x={x} y={y} showToolTip={this.showToolTip} hideToolTip={this.hideToolTip}/>
+                        <ToolTip tooltip={this.state.tooltip}/>
                     </g>
                 </svg>
             </div>
@@ -115,6 +170,7 @@ var Axis = React.createClass({
     	this.renderAxis(); 
     },
     renderAxis: function () {
+
         var node = ReactDOM.findDOMNode(this);
         d3.select(node).call(this.props.axis);
     },
@@ -157,6 +213,56 @@ var Grid = React.createClass({
  
 });
 
+var ToolTip = React.createClass({
+    propTypes: {
+        tooltip:React.PropTypes.object
+    },
+
+    render: function(){
+ 
+        var visibility="hidden";
+        var transform="";
+        var x=0;
+        var y=0;
+        var width=150,height=70;
+        var transformText='translate('+width/2+','+(height/2-5)+')';
+        var transformArrow="";
+ 
+        if(this.props.tooltip.display === true){
+            var position = this.props.tooltip.pos;
+ 
+            x = position.x;
+            y = position.y;
+            visibility = "visible";
+ 
+ 
+            if(y > height){
+                transform='translate(' + (x-width/2) + ',' + (y-height-20) + ')';
+                transformArrow='translate('+(width/2-20)+','+(height-2)+')';
+            }else if(y < height){
+ 
+                transform='translate(' + (x-width/2) + ',' + (Math.round(y)+20) + ')';
+                transformArrow='translate('+(width/2-20)+','+0+') rotate(180,20,0)';
+            }
+ 
+        }else{
+            visibility="hidden"
+        };
+ 
+        return(
+            <g transform={transform}>
+                <rect class="shadow" is width={width} height={height} rx="5" ry="5" visibility={visibility} fill="#6391da" opacity=".9"/>
+                <polygon class="shadow" is points="10,0  30,0  20,10" transform={transformArrow}
+                         fill="#6391da" opacity=".9" visibility={visibility}/>
+                <text is visibility={visibility} transform={transformText}>
+                    <tspan is x="0" text-anchor="middle" font-size="15px" fill="#ffffff">{this.props.tooltip.data.key}</tspan>
+                    <tspan is x="0" text-anchor="middle" dy="25" font-size="20px" fill="#a9f3ff">{this.props.tooltip.data.value+" visits"}</tspan>
+                </text>
+            </g>
+        );
+    }
+});
+
 
 var Dots = React.createClass({
 	
@@ -175,8 +281,16 @@ var Dots = React.createClass({
 			data.pop();
 
 		var circles = data.map(function(d,i){
+		var formatTime = d3.timeFormat("%b %e %Y")
+
+    			console.log(_this.props.x(d.date))
 			return (
-					<circle className = 'point' r="5" cx={_this.props.x(d.date)} cy={_this.props.y(d.count)} fill="#7dc7f4" stroke="#3f5175" strokeWidth="4px" key={i}/>
+					<circle className="dot" r="7" cx={_this.props.x(d.date)} cy={_this.props.y(d.count)} 
+					fill="#7dc7f4" stroke="#3f5175" strokeWidth="5px" key={i}
+                    onMouseOver={_this.props.showToolTip} 
+                    onMouseOut={_this.props.hideToolTip}
+                    data-key={formatTime(d.date)} 
+                    data-value={d.count}/>
 				)
 		});
 
